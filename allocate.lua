555f9ae880f6f8ec3a12d899197e5e5a7cef5aa3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-03 14:26:35 +0200

    one-off size fix

--- allocate.lua
+++ allocate.lua
@@ -2,6 +2,8 @@
 function blockexchange.allocate(playername, pos1, schema_uid)
   blockexchange.api.get_schema(schema_uid, function(schema)
     local pos2 = vector.add(pos1, {x=schema.size_x, y=schema.size_y, z=schema.size_z})
+    pos2 = vector.add(pos2, 1)
+
     local total_parts =
       math.ceil(math.abs(pos1.x - pos2.x) / blockexchange.part_length) *
       math.ceil(math.abs(pos1.y - pos2.y) / blockexchange.part_length) *
