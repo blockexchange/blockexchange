ea7965842032dd5d2bb2d0384273d9fede50a1b9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-03 11:34:17 +0200

    user mgmt stubs

---
+++ chat_user.lua
@@ -0,0 +1,19 @@
+
+
+minetest.register_chatcommand("bx_register", {
+	description = "",
+	func = function()
+  end
+})
+
+minetest.register_chatcommand("bx_login", {
+	description = "",
+	func = function()
+  end
+})
+
+minetest.register_chatcommand("bx_logout", {
+	description = "",
+	func = function()
+  end
+})
