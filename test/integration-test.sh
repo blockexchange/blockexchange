345b1e9a00d2150e7a5bf39fa2dbc08ed383d120
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-07 11:37:58 +0200

    fix path in tests

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+CWD=$(dirname $0)
+cd ${CWD}/../
+
 CFG=/tmp/minetest.conf
 MTDIR=/tmp/mt
 WORLDDIR=${MTDIR}/worlds/world
@@ -15,8 +18,8 @@ chmod 777 ${MTDIR} -R
 docker run --rm -i \
 	-v ${CFG}:/etc/minetest/minetest.conf:ro \
 	-v ${MTDIR}:/var/lib/minetest/.minetest \
-	-v $(pwd)/../:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
-  -v $(pwd)/test_mod/:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange_test \
+	-v $(pwd)/:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
+  -v $(pwd)/test/test_mod/:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange_test \
   --network host \
 	registry.gitlab.com/minetest/minetest/server:5.1.0
 
