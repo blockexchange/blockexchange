d0699a931bcd9f6f166962bc8e24ec9704da925e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-07 16:46:29 +0200

    hide placeholder node from inv

--- placeholder_node.lua
+++ placeholder_node.lua
@@ -4,7 +4,8 @@ minetest.register_node("blockexchange:placeholder", {
 	description = "Blockexchange paceholder node",
 	groups = {
     cracky=3,
-    oddly_breakable_by_hand=3
+    oddly_breakable_by_hand=3,
+		not_in_creative_inventory=1
   },
   tiles = {
     "unknown_node.png"
