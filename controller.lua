4cb32b957958cae55eaa2a8c31b3b6dc6694fcc1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-20 13:11:08 +0200

    draft out a few ideas on the controller node

--- controller.lua
+++ controller.lua
@@ -9,3 +9,25 @@ minetest.register_node("blockexchange:controller", {
 		oddly_breakable_by_hand=3
 	}
 })
+
+--[[
+
+UI:
+ * extends -x -y -z / +x +y +z
+ * Name
+ * Username (prefilled on "Upload")
+
+Features:
+ * Upload: uploads the region with the specified schema name
+ * Upload-Overwrite: uploads again and replaces the previous schema
+ * Download: downloads/updates/restores from remote
+ * Check: check if the remote schema is newer
+
+On upload:
+ * Save owner to controller-metadata *before* uploading
+ * Save schema timestamp to controller-metadata *before* uploading
+
+On download:
+ * Check size, if not equal: abort
+
+--]]
