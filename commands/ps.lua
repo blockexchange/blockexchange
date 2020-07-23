8df0ba8db2208f3f27818f1f990345fa8ba5de44
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-23 21:09:10 +0200

    fix nil error on /bx_ps

--- commands/ps.lua
+++ commands/ps.lua
@@ -10,7 +10,7 @@ minetest.register_chatcommand("bx_ps", {
 			local runtime_seconds = now - ctx._meta.start_time
       msg = "+ [" .. ctx._meta.id .. "]" ..
 				" Type" .. ctx.type ..
-				" Owner '" .. msg.playername .. "'" ..
+				" Owner '" .. ctx.playername .. "'" ..
 				" Runtime: " .. runtime_seconds .. " s"
     end
 		return true, msg
