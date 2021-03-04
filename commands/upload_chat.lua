1a0f7812200e77885f8c583466ce3e2c36b7959d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-03-04 17:44:35 +0100

    fix redundant protection check when "protection_bypass" priv is available

--- commands/upload_chat.lua
+++ commands/upload_chat.lua
@@ -4,7 +4,8 @@ minetest.register_chatcommand("bx_save", {
 	description = "Uploads the selected region to the blockexchange server",
 	func = function(name, schemaname)
     local has_protected_upload_priv = minetest.check_player_privs(name, { blockexchange_protected_upload = true })
-    local has_blockexchange_priv = minetest.check_player_privs(name, { blockexchange = true })
+		local has_blockexchange_priv = minetest.check_player_privs(name, { blockexchange = true })
+		local has_protection_bypass_priv = minetest.check_player_privs(name, { protection_bypass = true })
 
     if not has_blockexchange_priv and not has_protected_upload_priv then
 				return false, "Required privs: 'blockexchange' or 'blockexchange_protected_upload'"
@@ -27,7 +28,7 @@ minetest.register_chatcommand("bx_save", {
       return false, "you need to set /bx_pos1 and /bx_pos2 first!"
     end
 
-    if not has_blockexchange_priv and has_protected_upload_priv then
+    if not has_blockexchange_priv and has_protected_upload_priv and not has_protection_bypass_priv then
       -- kick off protection-check worker and add deferred upload context
       blockexchange.protectioncheck(name, pos1, pos2, schemaname)
     else
