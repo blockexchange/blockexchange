9bcf5e8e02c76bde7abed42e3cf5773c742b0935
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-05-11 09:57:47 +0200

    local operation

--- init.lua
+++ init.lua
@@ -1,13 +1,9 @@
-
+-- optional http instance
 local http = minetest.request_http_api()
 
-if not http then
-	minetest.log("error", "the 'blockexchange' mod needs access to the http api!")
-	return
-end
-
-
 blockexchange = {
+	-- online flag
+	is_online = http ~= nil,
 	api = {},
 	api_version_major = 1,
 	url = minetest.settings:get("blockexchange.url") or "https://blockexchange.minetest.land",
@@ -19,15 +15,21 @@ blockexchange = {
 	pos2 = {} -- name -> pos
 }
 
+if not blockexchange.is_online then
+	minetest.log("warning", "[blockexchange] the http api is not enabled, functionality is limited to local operations")
+end
+
 local MP = minetest.get_modpath("blockexchange")
 
 -- http api
-loadfile(MP.."/api/info.lua")(http, blockexchange.url)
-loadfile(MP.."/api/schema.lua")(http, blockexchange.url)
-loadfile(MP.."/api/schemapart.lua")(http, blockexchange.url)
-loadfile(MP.."/api/schemamods.lua")(http, blockexchange.url)
-loadfile(MP.."/api/searchschema.lua")(http, blockexchange.url)
-loadfile(MP.."/api/token.lua")(http, blockexchange.url)
+if blockexchange.is_online then
+	loadfile(MP.."/api/info.lua")(http, blockexchange.url)
+	loadfile(MP.."/api/schema.lua")(http, blockexchange.url)
+	loadfile(MP.."/api/schemapart.lua")(http, blockexchange.url)
+	loadfile(MP.."/api/schemamods.lua")(http, blockexchange.url)
+	loadfile(MP.."/api/searchschema.lua")(http, blockexchange.url)
+	loadfile(MP.."/api/token.lua")(http, blockexchange.url)
+end
 
 -- nodes
 dofile(MP.."/nodes/controller.lua")
@@ -50,28 +52,34 @@ dofile(MP.."/util/is_area_protected.lua")
 dofile(MP.."/util/iterator_next.lua")
 
 -- search
-dofile(MP.."/search/chat.lua")
-dofile(MP.."/search/formspec.lua")
+if blockexchange.is_online then
+	dofile(MP.."/search/chat.lua")
+	dofile(MP.."/search/formspec.lua")
+end
 
 -- commands
-dofile(MP.."/commands/api_check_wrapper.lua")
-dofile(MP.."/commands/info.lua")
-dofile(MP.."/commands/license.lua")
-dofile(MP.."/commands/pos.lua")
-dofile(MP.."/commands/user.lua")
-dofile(MP.."/commands/allocate.lua")
-dofile(MP.."/commands/allocate_chat.lua")
+if blockexchange.is_online then
+	dofile(MP.."/commands/api_check_wrapper.lua")
+	dofile(MP.."/commands/info.lua")
+	dofile(MP.."/commands/license.lua")
+	dofile(MP.."/commands/user.lua")
+	dofile(MP.."/commands/allocate.lua")
+	dofile(MP.."/commands/allocate_chat.lua")
+	dofile(MP.."/commands/pos.lua")
+	dofile(MP.."/commands/upload.lua")
+	dofile(MP.."/commands/upload_chat.lua")
+	dofile(MP.."/commands/download.lua")
+	dofile(MP.."/commands/download_chat.lua")
+end
 dofile(MP.."/commands/emerge.lua")
 dofile(MP.."/commands/emerge_chat.lua")
-dofile(MP.."/commands/upload.lua")
-dofile(MP.."/commands/upload_chat.lua")
 dofile(MP.."/commands/protectioncheck.lua")
-dofile(MP.."/commands/download.lua")
-dofile(MP.."/commands/download_chat.lua")
 
 -- worker functions
-dofile(MP.."/worker/download_worker.lua")
-dofile(MP.."/worker/upload_worker.lua")
+if blockexchange.is_online then
+	dofile(MP.."/worker/download_worker.lua")
+	dofile(MP.."/worker/upload_worker.lua")
+end
 dofile(MP.."/worker/emerge_worker.lua")
 dofile(MP.."/worker/protectioncheck_worker.lua")
 
