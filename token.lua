163eddebea695ffbac2ef6b136e6f573a2853065
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-05-27 19:13:36 +0200

    use minetest.decode_base64

--- token.lua
+++ token.lua
@@ -3,35 +3,13 @@
 
 local META_KEY = "blockexchange_token"
 
--- http://lua-users.org/wiki/BaseSixtyFour
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
---- decode a base64 string into a string
--- @param the data in base64 format
--- @return the decoded string
--- TODO: check if minetest.decode_base64 can work here
-local function dec(data)
-	data = string.gsub(data, '[^'..b..'=]', '')
-	return (data:gsub('.', function(x)
-		if (x == '=') then return '' end
-		local r,f='',(b:find(x)-1)
-		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-		return r;
-	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-		if (#x ~= 8) then return '' end
-		local c=0
-		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-		return string.char(c)
-	end))
-end
-
 --- parses a jwt token
 -- @param the token in string format
 -- @return the payload in json format
 function blockexchange.parse_token(token)
 	-- header.payload.signature
 	local _, _, _, payload_json = string.find(token, "^([^.]+).([^.]+).([^.]+)$")
-	local json = dec(payload_json)
+	local json = minetest.decode_base64(payload_json)
 	local payload = minetest.parse_json(json)
 	return payload
 end
