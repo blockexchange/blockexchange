7b14d813fab35e597697d706fd6a17b98796879e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-02-29 13:23:04 +0100

    expose `blockexchange.mark_changed(pos1, pos2)` for change tracking

--- autosave.lua
+++ autosave.lua
@@ -69,7 +69,7 @@ minetest.after(1, worker)
 
 -- change tracking code below
 
-local function deferred_export(pos1, pos2)
+function blockexchange.mark_changed(pos1, pos2)
     pos1, pos2 = blockexchange.sort_pos(pos1, pos2)
 
     local mapblock_pos1 = blockexchange.get_mapblock(pos1)
@@ -88,20 +88,20 @@ end
 -- autosave on minetest.set_node
 local old_set_node = minetest.set_node
 function minetest.set_node(pos, node)
-    deferred_export(pos, pos)
+    blockexchange.mark_changed(pos, pos)
     return old_set_node(pos, node)
 end
 
 -- autosave on minetest.swap_node
 local old_swap_node = minetest.swap_node
 function minetest.swap_node(pos, node)
-    deferred_export(pos, pos)
+    blockexchange.mark_changed(pos, pos)
     return old_swap_node(pos, node)
 end
 
 -- autosave on place/dignode
 local function place_dig_callback(pos)
-    deferred_export(pos, pos)
+    blockexchange.mark_changed(pos, pos)
 end
 minetest.register_on_placenode(place_dig_callback)
 minetest.register_on_dignode(place_dig_callback)
@@ -111,14 +111,14 @@ if minetest.get_modpath("worldedit") then
     -- used by various primitives and commands
     local old_mapgenhelper_init = worldedit.manip_helpers.init
     worldedit.manip_helpers.init = function(pos1, pos2)
-        deferred_export(pos1, pos2)
+        blockexchange.mark_changed(pos1, pos2)
         return old_mapgenhelper_init(pos1, pos2)
     end
 
     -- used by //load and others
     local old_keeploaded = worldedit.keep_loaded
     worldedit.keep_loaded = function(pos1, pos2)
-        deferred_export(pos1, pos2)
+        blockexchange.mark_changed(pos1, pos2)
         return old_keeploaded(pos1, pos2)
     end
 end
@@ -131,7 +131,7 @@ minetest.register_on_mods_loaded(function()
             local old_on_receive_fields = def.on_receive_fields
             minetest.override_item(nodename, {
                 on_receive_fields = function(pos, formname, fields, sender)
-                    deferred_export(pos, pos)
+                    blockexchange.mark_changed(pos, pos)
                     return old_on_receive_fields(pos, formname, fields, sender)
                 end
             })
@@ -142,7 +142,7 @@ minetest.register_on_mods_loaded(function()
             local old_inv_move = def.on_metadata_inventory_move
             minetest.override_item(nodename, {
                 on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-                    deferred_export(pos, pos)
+                    blockexchange.mark_changed(pos, pos)
                     return old_inv_move(pos, from_list, from_index, to_list, to_index, count, player)
                 end
             })
@@ -153,7 +153,7 @@ minetest.register_on_mods_loaded(function()
             local old_inv_put = def.on_metadata_inventory_put
             minetest.override_item(nodename, {
                 on_metadata_inventory_put = function(pos, listname, index, stack, player)
-                    deferred_export(pos, pos)
+                    blockexchange.mark_changed(pos, pos)
                     return old_inv_put(pos, listname, index, stack, player)
                 end
             })
@@ -164,7 +164,7 @@ minetest.register_on_mods_loaded(function()
             local old_inv_take = def.on_metadata_inventory_take
             minetest.override_item(nodename, {
                 on_metadata_inventory_take = function(pos, listname, index, stack, player)
-                    deferred_export(pos, pos)
+                    blockexchange.mark_changed(pos, pos)
                     return old_inv_take(pos, listname, index, stack, player)
                 end
             })
