2cdaa7d3519ca5718597696066f72c76193e60f4
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-28 21:00:21 +0100

    wip

--- commands/media.lua
+++ commands/media.lua
@@ -1,5 +1,7 @@
 
 function blockexchange.upload_mod_media(token, modname, license, source)
+    local modpath = minetest.get_modpath(modname)
+
     return blockexchange.api.create_media_mod(token, {
         name = modname,
         source = source or "",
@@ -10,6 +12,14 @@ function blockexchange.upload_mod_media(token, modname, license, source)
             local mn = string.gmatch(nodename, "([^:]+)")()
             if mn == modname then
                 -- TODO: create nodedef
+            end
+        end
+
+        for _, foldername in ipairs({"textures", "models"}) do
+            local texturepath = modpath .. "/" .. foldername
+            local dir_list = minetest.get_dir_list(texturepath)
+            for _, filename in pairs(dir_list) do
+                -- TODO: texturepath .. "/" .. filename
             end
         end
 
