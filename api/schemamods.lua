e2fd77ae405eaf06f17465c33d935dbbd4c524ba
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-04-02 10:21:16 +0200

    promise http utils

--- api/schemamods.lua
+++ api/schemamods.lua
@@ -1,24 +1,11 @@
 local http, url = ...
 
 function blockexchange.api.create_schemamods(token, schema_uid, mod_names)
-  return Promise.new(function(resolve, reject)
-    local json = minetest.write_json(mod_names);
-    http.fetch({
-      url = url .. "/api/schema/" .. schema_uid .. "/mods",
-      extra_headers = {
-        "Content-Type: application/json",
-        "Authorization: " .. token
-      },
-      timeout = 5,
-      post_data = json
-    }, function(res)
-      if res.succeeded and (res.code >= 200 or res.code < 300) then
-        resolve(true)
-      else
-        reject(res.code or 0)
-      end
-    end)
-  end)
+  return Promise.http(http, url .. "/api/schema/" .. schema_uid .. "/mods", {
+    method = "POST",
+    data = mod_names,
+    headers = { "Authorization: " .. token }
+  }):next(function(res) return res.code == 200 end)
 end
 
 function blockexchange.api.get_schemamods(schema_uid)
