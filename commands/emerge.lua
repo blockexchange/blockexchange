e4f5919bae0314545e1c1defdd46f33a4bb64a61
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-02-03 13:42:23 +0100

    asyncify emerge

--- commands/emerge.lua
+++ commands/emerge.lua
@@ -9,24 +9,36 @@
 -- @return a promise that resolves if the operation is complete
 -- @return the job context
 function blockexchange.emerge(playername, pos1, pos2)
-  local total_parts = blockexchange.count_schemaparts(pos1, pos2)
-  local iterator = blockexchange.iterator(pos1, pos1, pos2)
 
   local ctx = {
     type = "emerge",
     playername = playername,
-    pos1 = pos1,
-    pos2 = pos2,
-    iterator = iterator,
-    current_pos = iterator(),
-    current_part = 0,
-    progress_percent = 0,
-    total_parts = total_parts,
-    promise = Promise.new()
+    progress_percent = 0
   }
 
-  -- start emerge worker with context
-  blockexchange.emerge_worker(ctx)
+  local promise = Promise.async(function(await)
+    local total_parts = 0
 
-  return ctx.promise, ctx
+    for current_pos, _, progress in blockexchange.iterator(pos1, pos1, pos2) do
+      local current_pos2 = vector.add(current_pos, 15)
+
+      ctx.progress_percent = math.floor(progress * 100 * 10) / 10
+      blockexchange.log(
+        "action",
+        "emerging area at " .. minetest.pos_to_string(current_pos) .. " progress: " .. ctx.progress_percent
+      )
+      await(Promise.emerge_area(current_pos, current_pos2))
+      total_parts = total_parts + 1
+
+      if ctx.cancel then
+        error("canceled", 0)
+      end
+
+      await(Promise.after(blockexchange.min_delay))
+    end
+
+    return total_parts
+  end)
+
+  return promise, ctx
 end
