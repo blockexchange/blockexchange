5d28b3d50c348038dd2a38e5944e0892b11ebaf2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-09-18 18:07:26 +0200

    add nodecore dynalight to ignored nodes

--- util/ignored_content_ids.lua
+++ util/ignored_content_ids.lua
@@ -6,6 +6,10 @@ blockexchange.ignored_content_ids = {}
 --- add a nodename to ignore (set to air on save)
 -- @param nodename the nodename to ignore
 function blockexchange.ignore_node(nodename)
+    if not minetest.registered_nodes[nodename] then
+        -- node not available
+        return
+    end
     local node_id = minetest.get_content_id(nodename)
     blockexchange.ignored_content_ids[node_id] = true
 end
@@ -17,4 +21,11 @@ blockexchange.ignore_node("ignore")
 -- ignore vacuum
 if minetest.get_modpath("vacuum") then
     blockexchange.ignore_node("vacuum:vacuum")
+end
+
+if minetest.get_modpath("nc_api_active") then
+    blockexchange.ignore_node("nc_api_active:shade")
+    for i = 1, 14 do
+        blockexchange.ignore_node("nc_api_active:light" .. i)
+    end
 end
